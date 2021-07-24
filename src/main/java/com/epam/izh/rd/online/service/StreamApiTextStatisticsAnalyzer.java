9597c16a40d51c48e232b28201346530eb6eeec1
alexandr <iuken067@gmail.com>
2021-07-24 23:27:05 +0400

    Implement with stream

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,10 +2,8 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.*;
 
@@ -16,36 +14,55 @@ import static java.util.Collections.*;
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return getWords(text).
+                stream().
+                mapToInt((s) -> s.length()).
+                sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return (int) getWords(text).stream().count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return (int) getUniqueWords(text).stream().count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        return Arrays.stream(text.split("\\W+")).collect(Collectors.toList());
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        return getWords(text).stream().distinct().collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        return getWords(text)
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                (s) -> s,
+                                (s) -> 1, Integer::sum));
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        switch (direction){
+            case ASC:{
+                return getWords(text).stream().sorted((a, b) -> a.length() - b.length()).collect(Collectors.toList());
+            }
+            case DESC:{
+                return getWords(text).stream().sorted((a, b) -> b.length() - a.length()).collect(Collectors.toList());
+            }
+            default: {
+                return emptyList();
+            }
+
+        }
     }
 }
